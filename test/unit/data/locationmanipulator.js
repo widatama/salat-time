f1850c826c5c57454298602db39d424447549404
Danu Widatama <widatama@gmail.com>
2016-08-19 08:58:57 +0700

    Update unit tests for location manipulator

--- test/unit/data/locationmanipulator.js
+++ test/unit/data/locationmanipulator.js
@@ -1,37 +1,68 @@
 import tape from "tape";
 import locationManipulator from "../../../app/javascripts/data/locationmanipulator";
 
-tape("Location data manipulator", (assert) => {
-  // This is the location data sample fetched from freegeoip
-  let rawLocation = {
-    ip: "202.62.16.22",
-    country_code: "ID",
-    country_name: "Indonesia",
-    region_code: "JK",
-    region_name: "Jakarta",
-    city: "Jakarta",
-    zip_code: "",
-    time_zone: "Asia/Jakarta",
-    latitude: -6.1744,
-    longitude: 106.8294,
-    metro_code: 0
-  };
+tape("Location data manipulator", function(t) {
 
-  // Transform the raw location data to get only what is needed with the correct format
-  let location = locationManipulator.transformLocationResponse(rawLocation);
+  t.test("Transform freegeoip response", (assert) => {
+    // This is the location data sample fetched from freegeoip
+    let rawLocation = {
+      ip: "202.62.16.22",
+      country_code: "ID",
+      country_name: "Indonesia",
+      region_code: "JK",
+      region_name: "Jakarta",
+      city: "Jakarta",
+      zip_code: "",
+      time_zone: "Asia/Jakarta",
+      latitude: -6.1744,
+      longitude: 106.8294,
+      metro_code: 0
+    };
 
-  // Test the result location
-  assert.ok(location.country, "Country exists");
-  assert.equal(typeof location.country, "string", "Country is a string");
-  assert.ok(location.country, "City exists");
-  assert.equal(typeof location.city, "string", "City is a string");
-  assert.ok(location.country, "Time zone exists");
-  assert.equal(typeof location.timezone, "string", "Time zone is a string");
-  assert.ok(location.latitude, "Latitude exists");
-  assert.equal(typeof location.latitude, "number", "Latitude is a number");
-  assert.ok(location.longitude, "Longitude exists");
-  assert.equal(typeof location.longitude, "number", "Longitude is a number");
+    // Transform the raw location data to get only what is needed with the correct format
+    let location = locationManipulator.transformIPLocationResponse(rawLocation);
 
-  assert.end();
+    assert.plan(10);
 
+    // Test the result location
+    assert.ok(location.country, "Country exists");
+    assert.equal(typeof location.country, "string", "Country is a string");
+    assert.ok(location.country, "City exists");
+    assert.equal(typeof location.city, "string", "City is a string");
+    assert.ok(location.country, "Time zone exists");
+    assert.equal(typeof location.timezone, "string", "Time zone is a string");
+    assert.ok(location.latitude, "Latitude exists");
+    assert.equal(typeof location.latitude, "number", "Latitude is a number");
+    assert.ok(location.longitude, "Longitude exists");
+    assert.equal(typeof location.longitude, "number", "Longitude is a number");
+  });
+
+  t.test("Transform nominatim response", (assert) => {
+    // This is the reverse geolocation data sample fetched from nominatim
+    let response = {
+      address: {
+        country: "Indonesia",
+        city: "Jakarta"
+      },
+      lat: -6.1744,
+      lon: 106.8294
+    };
+
+    // Transform the response data to get only what is needed with the correct format
+    let location = locationManipulator.transformReverseGeolocationResponse(response);
+
+    assert.plan(8);
+
+    // Test the result location
+    assert.ok(location.country, "Country exists");
+    assert.equal(typeof location.country, "string", "Country is a string");
+    assert.ok(location.country, "City exists");
+    assert.equal(typeof location.city, "string", "City is a string");
+    assert.ok(location.latitude, "Latitude exists");
+    assert.equal(typeof location.latitude, "number", "Latitude is a number");
+    assert.ok(location.longitude, "Longitude exists");
+    assert.equal(typeof location.longitude, "number", "Longitude is a number");
+  });
+
+  t.end();
 });
