00a2335c21b6ecefc184c71724d8ddc809b74d1f
Danu Widatama <widatama@gmail.com>
2018-05-19 20:55:37 +0700

    Set up workbox

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
 
 const OptimizeCSSPlugin = require("optimize-css-assets-webpack-plugin");
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const WebpackStylish = require('webpack-stylish');
 
 const webpackConfig = require('./webpack.config');
 const appConfig = require('./app.config');
+
+const WorkboxPluginConfig = {
+  swDest: path.resolve('', `./${appConfig.paths.dist.path}/sw.js`),
+  clientsClaim: true,
+  skipWaiting: true,
+};
 
 webpackConfig.mode = 'production';
 webpackConfig.stats = 'none';
@@ -19,6 +26,7 @@ webpackConfig.plugins = (webpackConfig.plugins || []).concat([
   new OptimizeCSSPlugin({
     cssProcessorOptions: { discardComments: { removeAll: true } },
   }),
+  new WorkboxPlugin.GenerateSW(WorkboxPluginConfig),
   new WebpackStylish(),
 ]);
 
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,13 +3,20 @@ import Vue from 'vue';
 import '../stylesheets/app.css';
 
 import App from './components/app.vue';
-
 import store from './modules/store/';
 
-Vue.component('app', App);
-
 const salatTime = new Vue({
+  name: 'SalatTime',
   el: '#vue',
+  components: {
+    App,
+  },
   store,
   render: h => h(App),
 });
+
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in global.navigator) {
+  global.addEventListener('load', () => {
+    global.navigator.serviceWorker.register('sw.js');
+  });
+}
