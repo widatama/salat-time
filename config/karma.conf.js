477401d0d9a06ebee5921f737f0425163000a96b
Danu Widatama <widatama@gmail.com>
2019-09-22 17:37:57 +0700

    Update testing related dependencies

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,8 +1,9 @@
-const karmaChromeLauncher = require('karma-chrome-launcher');
+/* eslint-disable import/no-extraneous-dependencies */
 const karmaFirefoxLauncher = require('karma-firefox-launcher');
 const karmaTap = require('karma-tap');
 const karmaTapPrettyReporter = require('karma-tap-pretty-reporter');
 const karmaWebpack = require('karma-webpack');
+/* eslint-enable import/no-extraneous-dependencies */
 
 // Use the exact same webpack config by requiring it
 // but make sure to delete the normal entry
@@ -18,22 +19,19 @@ webpackConf.stats = 'none';
 
 module.exports = config => {
   config.set({
-    plugins: [
-      karmaFirefoxLauncher,
-      karmaTap,
-      karmaTapPrettyReporter,
-      karmaWebpack,
-    ],
-    browsers: ['Firefox'],
+    plugins: [karmaFirefoxLauncher, karmaTap, karmaTapPrettyReporter, karmaWebpack],
+    browsers: ['FirefoxHeadless'],
     frameworks: ['tap'],
     reporters: ['tap-pretty'],
     tapReporter: {
       prettifier: 'tap-summary',
     },
-    files: [ // entry file for all tests
+    files: [
+      // entry file for all tests
       '../src/javascripts/test_entry.js',
     ],
-    preprocessors: { // pass the entry file to webpack for bundling
+    preprocessors: {
+      // pass the entry file to webpack for bundling
       '../src/javascripts/test_entry.js': ['webpack'],
     },
     client: {
